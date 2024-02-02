f4e0d4df245c527d9ed29d4d0ee6b0c0bd7c24bf
KEY <gl176705@gmail.com>
2024-02-03 06:49:37 +0900

    Tweetボタン追加

--- content-script.js
+++ content-script.js
@@ -1,6 +1,15 @@
-function handlerCWP(e) {
+function handlerLcwpEleSelect(e) {
   let category = e.target.value;
   navigator.clipboard.writeText(category);
+  return;
+}
+
+function handlerLcwpEleButton(e) {
+  let valueText = document.querySelector('#edit-broadcast-title-formgroup').value;
+  let valueUrl = "https://www.twitch.tv/" + document.baseURI.split("/")[4];
+  let base = "https://twitter.com/intent/tweet?";
+  let query = [["text", encodeURIComponent(valueText)].join("="), ["url", encodeURIComponent(valueUrl)].join("=")].join("&");
+  window.open(base + query, "_brank");
   return;
 }
 
@@ -8,15 +17,17 @@ var eleDiv = document.createElement('div');
 eleDiv.innerHTML = '\
 <form>\
 選択するとクリップボードにエントリー<br>\
-  <select id="eleCWP">\
+  <select id="lcwpEleSelect">\
     <option value="-">-</option>\
     <option value="Tetris">テトリス</option>\
     <option value="Ketsui: Kizuna Jigoku Tachi">ケツイ</option>\
     <option value="Ultima Online">UO</option>\
     <option value="Street Fighter Alpha 3">ゼロ3</option>\
     <option value="screeps">screeps</option>\
-  </select>\
+  </select><br>\
+  <input id="lcwpEleButton" type="button" value="Tweet">\
 </form>\
 ';
 document.querySelector('.tw-transition-group').appendChild(eleDiv)
-document.querySelector('#eleCWP').onchange = handlerCWP;
+document.querySelector('#lcwpEleSelect').onchange = handlerLcwpEleSelect;
+document.querySelector('#lcwpEleButton').onclick = handlerLcwpEleButton;
